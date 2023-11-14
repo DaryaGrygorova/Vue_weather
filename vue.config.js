249a712a7f7b69a0ce0f985dd8094fa69eab8a14
DaryaGrygorova <grygorova.darya@gmail.com>
2023-11-14 22:41:18 +0200

    fix deploy

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: "docs",
-  // publicPath: process.env.VUE_APP_NODE_ENV === "production" ? "/Vue_weather/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/Vue_weather/" : "/",
 });
