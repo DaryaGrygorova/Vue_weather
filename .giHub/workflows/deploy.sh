da390ad201973a4001ab3616942be66dd9bdfceb
DaryaGrygorova <grygorova.darya@gmail.com>
2023-11-14 22:23:24 +0200

    Refactor menu

--- .giHub/workflows/deploy.sh
+++ .giHub/workflows/deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:DaryaGrygorova/Vue_weather.git master:gh-pages
+git push -f git@github.com:daryagrygorova/Vue_weather.git master:gh-pages
 
 cd -
--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,7 @@
+<script lang="ts" setup>
+import Menu from "./components/menu-comp.vue";
+</script>
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/weather">Weather</router-link>
-  </nav>
-  <router-view />
+  <Menu />
 </template>
-
-<style lang="scss" scoped>
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
