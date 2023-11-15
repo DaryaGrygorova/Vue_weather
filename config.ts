6fde6ecb5cc7704d469fb15f79afeb09b26f5569
DaryaGrygorova <grygorova.darya@gmail.com>
2023-11-15 11:00:08 +0200

    new build (new confjg)

--- config.ts
+++ config.ts
@@ -1,2 +1,2 @@
-export const API_KEY = "04f07c499bfaf8b2b85bf866099fa06a";
+export const API_KEY = process.env.VUE_APP_API_KEY;
 export const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
