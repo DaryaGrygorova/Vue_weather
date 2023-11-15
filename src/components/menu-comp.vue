388da217e2577c7c24dba66df21d9fe50f3b4361
DaryaGrygorova <grygorova.darya@gmail.com>
2023-11-15 10:48:15 +0200

    Refactor dependencies, add welcome page content

--- src/components/menu-comp.vue
+++ src/components/menu-comp.vue
@@ -1,21 +1,27 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">About</router-link>
     <router-link to="/weather">Weather</router-link>
   </nav>
   <router-view />
 </template>
 
 <style lang="scss" scoped>
+@import "@/assets/styles/main.scss";
+
 nav {
   padding: 30px;
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: $text-primary;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: $text-accent;
+    }
+
+    &:not(:last-child) {
+      margin-right: 20px;
     }
   }
 }
--- src/components/welcome-comp.vue
+++ src/components/welcome-comp.vue
@@ -1,27 +1,45 @@
 <template>
-  <div class="hello">
-    <h1>Welcome to my Weather App on Vue.js</h1>
+  <div class="page">
+    <div class="container">
+      <section class="section about">
+        <h1>Welcome to my Weather App on Vue.js!</h1>
+        <article>
+          <p>
+            This intuitive web application provides you with weather forecasts.
+            User can search for weather information of different places. By
+            default the App displays the local weather at Kyiv city, Ukraine.
+            Weather icon changes depending on weather conditions.
+          </p>
+
+          <h3>Features:</h3>
+          <ul>
+            <li>
+              <b>Intuitive Design:</b> The user-friendly interface makes it easy
+              to access and understand weather information.
+            </li>
+            <li>
+              <b>City-Based Weather Forecast:</b> Get accurate and detailed
+              weather forecasts for your city.
+            </li>
+            <li>
+              <b>Responsive and Dynamic:</b> Seamlessly adapts to different
+              screen sizes for a consistent experience across devices.
+            </li>
+          </ul>
+          <h3>Technologies:</h3>
+          <ul>
+            <li>Vue.js (Vue3)</li>
+            <li>TypeScript</li>
+            <li>ESLint</li>
+            <li><a src="https://openweathermap.org/">Weather API</a></li>
+          </ul>
+        </article>
+      </section>
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
 @import "@/assets/styles/main.scss";
-
-* {
-  color: $text-primary;
-}
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+@import "@/assets/styles/welcome.scss";
 </style>
